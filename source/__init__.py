9feaefd0716cffc109392be1dd284bcbf77c1153
chloeskt <chloe.sekkat@ensae.fr>
2022-04-13 10:13:20 +0200

    add viz function visualize_generator_outputs

--- source/__init__.py
+++ source/__init__.py
@@ -1,4 +1,4 @@
 from .data import mnist_transforms
-from .utils import show_mnist_data, set_seed, train_ot_gan
+from .utils import show_mnist_data, set_seed, train_ot_gan, visualize_generator_outputs
 from .models import OTGANGenerator, OTGANCritic
 from .sinkhorn import MinibatchEnergyDistance, NewMinibatchEnergyDistance
